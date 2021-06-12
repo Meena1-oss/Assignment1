da33cd791eec14e9a20a35b4e2a7a96f6d248685
Meena1-oss <85676845+Meena1-oss@users.noreply.github.com>
2021-06-12 15:46:22 +0530

    Add files via upload

---
+++ Pro4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pro3 {
+
+	public static void main(String[] args) {
+		int num=5,i=1,j=1;
+		while(i<=num){
+			while(j<=i){
+				System.out.println("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
+
+
